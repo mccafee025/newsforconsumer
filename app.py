66570e076b79b027fe13bf7a9537a101167aa17d
mccafee025 <ademaamanka@gmail.com>
2022-05-04 11:47:06 +0300

    add top headlines from api

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     newsapi = NewsApiClient(api_key="a953af35750f44a29beae0b8c83e6d5e")
+    top_headlines = newsapi.get_top_headlines(sources = 'cnn')
