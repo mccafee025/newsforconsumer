aaf40986151dda624c6595d47f2e692defe4e9ba
mccafee025 <ademaamanka@gmail.com>
2022-05-04 11:50:15 +0300

    make a list of contents to store values

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ def home():
     newsapi = NewsApiClient(api_key="a953af35750f44a29beae0b8c83e6d5e")
     top_headlines = newsapi.get_top_headlines(sources = 'cnn')
     t_articles = top_headlines['articles']
+    news = []
+    dets = []
+    img = []
+    p_date = []
+    url = []
